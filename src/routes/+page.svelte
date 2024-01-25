ba182e1571f84eb9460934871541a2c68e0ca7f6
Jaco Kok <kokjaco2@gmail.com>
2024-01-25 19:44:03 +0200

    Added logo and created basic coming soon page.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,49 @@
-<script lang="ts">
-	import { Button } from "@kayord/ui";
-</script>
+<section class="flex h-screen flex-col items-center justify-center md:px-6 lg:px-8">
+	<figure>
+		<section class="img-bg" />
+		<img src="/logo.svg" alt="logo" class="mb-8" />
+	</figure>
+	<h1 class="text-4xl font-bold text-gray-900 dark:text-gray-100 md:text-5xl lg:text-6xl">
+		Coming Soon
+	</h1>
+	<p
+		class="mt-4 max-w-lg text-center text-lg text-gray-700 dark:text-gray-300 md:text-xl lg:text-2xl"
+	>
+		The future's codeword? Kayord
+	</p>
+</section>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-
-<Button>Button</Button>
+<style lang="postcss">
+	figure {
+		@apply relative flex flex-col;
+	}
+	figure img,
+	.img-bg {
+		@apply h-64 w-64 md:h-80 md:w-80;
+	}
+	.img-bg {
+		@apply absolute z-[-1] rounded-full blur-[50px] transition-all;
+		animation:
+			pulse 5s cubic-bezier(0, 0, 0, 0.5) infinite,
+			glow 5s linear infinite;
+	}
+	@keyframes glow {
+		0% {
+			@apply bg-primary/50;
+		}
+		33% {
+			@apply bg-secondary/50;
+		}
+		66% {
+			@apply bg-accent/50;
+		}
+		100% {
+			@apply bg-primary/50;
+		}
+	}
+	@keyframes pulse {
+		50% {
+			transform: scale(1.5);
+		}
+	}
+</style>
